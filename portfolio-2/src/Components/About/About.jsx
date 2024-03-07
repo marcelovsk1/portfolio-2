7c96566aaaba5a72f7a62d6cd9236180357ba169
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-07 15:43:45 -0300

    about

--- portfolio-2/src/Components/About/About.jsx
+++ portfolio-2/src/Components/About/About.jsx
@@ -8,39 +8,36 @@ const About = () => {
   return (
     <section className='styles_container'>
       <h2 className='styles_title'>About <span>Me</span></h2>
-      <div className='styles.content'>
-      <img src={About_Pic} alt="About_Pic" className='About-Pic' />
-        <ul className='styles.itens'>
+      <div className='styles_content'>
+        <img src={About_Pic} alt="About_Pic" className='About-Pic' />
+        <ul className='styles_itens'>
           <li className='education'>
             <div className='webdev'>
               <div className='description'>
                 <h3>Web-Developer</h3>
-                  <p>Full Stack Web Developer graduated from Le Wagon Montreal.
-                     With a solid foundation in both front-end and back-end technologies,
-                      I am dedicated to crafting impactful and functional digital experiences.</p>
-                </div>
-                <a href="https://www.lewagon.com">
-              <img src={le_wagon} alt="Le Wagon" className='le_wagon' />
-              </a>
+                <p>Full Stack Web Developer graduated from Le Wagon Montreal. With a solid foundation in both front-end and back-end technologies, I am dedicated to crafting impactful and functional digital experiences.</p>
+              </div>
             </div>
-            </li>
-            <li className='education'>
+              <a href="https://www.lewagon.com">
+                <img src={le_wagon} alt="Le Wagon" className='le_wagon' />
+              </a>
+          </li>
+          <li className='education'>
             <div className="webdev">
               <div className="description">
                 <h3>iOS Developer</h3>
-                  <p>Following my bootcamp graduation, I delved into iOS development,
-                    honing my skills and specializing in crafting seamless
-                    experiences for Apple's ecosystem.</p>
-                </div>
-                  <a href="https://www.linkedin.com/feed/update/urn:li:activity:7143249482875396096/">
-                  <img src={iOS} alt="iOS" className='iOS' />
-                </a>
+                <p>Following my bootcamp graduation, I delved into iOS development, honing my skills and specializing in crafting seamless experiences for Apple's ecosystem.</p>
             </div>
-            </li>
-            </ul>
+              </div>
+              <a href="https://www.linkedin.com/feed/update/urn:li:activity:7143249482875396096/">
+                <img src={iOS} alt="iOS" className='iOS' />
+              </a>
+          </li>
+        </ul>
       </div>
-      </section>
-  )
+    </section>
+  );
 };
+
 
 export default About
