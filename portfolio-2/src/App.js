3e7f37a98d86a22acb4bd368980d88fa5e21f54f
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-07 15:08:49 -0300

    intro

--- portfolio-2/src/App.js
+++ portfolio-2/src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./Components//Navbar/Navbar"
 import './App.css'
-// import Intro from "./Components//Intro/Intro";
+import Intro from "./Components//Intro/Intro";
 // import About from "./Components//About/About"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      {/* <Intro /> */}
+      <Intro />
       {/* <About /> */}
     </div>
   );
