c89158b691884f22cb8ecc84a7d44b33dce48306
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-08 09:21:09 -0300

    experience

--- portfolio-2/src/Components/Experience/Experience.jsx
+++ portfolio-2/src/Components/Experience/Experience.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styles from "./Experience.module.css"
+
+import JS from '../../../src/Assets/Skills/js.png';
+import Swift from '../../../src/Assets/Skills/swift.png';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>My <span>Experience</span></h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+            <div className={styles.skill}>
+              <div className='skills-item'>
+                <img src={JS} alt="js"/>
+              </div>
+              <p>JS</p>
+            </div>
+            <div className='skills-item'>
+                <img src={Swift} alt="js" className/>
+              </div>
+              <p>Swift</p>
+            </div>
+        </div>
+        <ul>
+
+        </ul>
+    </section>
+  );
 }
 
-export default Experience
+export default Experience;
